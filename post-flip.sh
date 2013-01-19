229561a028aed70d875cb5a679fea1356d30e4a3
Tomasz Rakowski <akayami@gmail.com>
2013-01-18 21:37:36 -0500

    Removed postflip

--- post-flip.sh
+++ post-flip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PROJECT_HOME=$1;
+#PROJECT_HOME=$1;
 
 
-sudo ln -f -s $PROJECT_HOME/bvrctl.sh /usr/bin/bvrctl.sh
-sudo ln -f -s $PROJECT_HOME/bvrdpl.sh /usr/bin/bvrdpl.sh
-sudo ln -f -s $PROJECT_HOME/bvrflip.sh /usr/bin/bvrflip.sh
-sudo ln -f -s $PROJECT_HOME/bvrstat.sh /usr/bin/bvrstat.sh		
+#sudo ln -f -s $PROJECT_HOME/bvrctl.sh /usr/bin/bvrctl.sh
+#sudo ln -f -s $PROJECT_HOME/bvrdpl.sh /usr/bin/bvrdpl.sh
+#sudo ln -f -s $PROJECT_HOME/bvrflip.sh /usr/bin/bvrflip.sh
+#sudo ln -f -s $PROJECT_HOME/bvrstat.sh /usr/bin/bvrstat.sh		
